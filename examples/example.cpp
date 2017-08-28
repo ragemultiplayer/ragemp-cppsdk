35cca2dc80478e3db49abb3b943f3fac4a828493
ragemp <ragemp@users.noreply.github.com>
2017-08-29 02:59:44 +0300

    Create example.cpp

---
+++ examples/example.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "../rage.hpp"
+
+namespace gm
+{
+	class EventHandler
+		: public rage::IEventHandler,
+		public rage::IPlayerHandler,
+		public rage::ITickHandler
+	{
+	public:
+		virtual rage::ITickHandler *GetTickHandler() { return this; }
+		virtual rage::IPlayerHandler *GetPlayerHandler() { return this; }
+
+		virtual void OnPlayerJoin(rage::IPlayer *player) { std::cout << "Player: " << player->GetId() << std::endl; }
+
+		virtual void Tick() { std::cout << "tick!!!!" << std::endl; }
+	};
+}
+
+RAGE_API rage::IPlugin *InitializePlugin(rage::IMultiplayer *mp)
+{
+	mp->AddEventHandler(new gm::EventHandler);
+	return new rage::IPlugin;
+}
